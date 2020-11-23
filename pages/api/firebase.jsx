22af7e6c62f113698b8fa414d643f174bb4ca96e
Boat-Programmer <53931555+Boat-Programmer@users.noreply.github.com>
2020-11-23 18:07:07 +0700

    Updated version firebase

--- pages/api/firebase.jsx
+++ pages/api/firebase.jsx
@@ -3,7 +3,7 @@ import firebase from "firebase";
 const firebaseProjectName = "b-cloud-app";
 
 const config = {
-  apiKey: "AIzaSyDn2ythD5-lSEwPUWHKE3nxKMw6ctw49y0",
+  apiKey: "M4vwGufRaHhaTtefCixty6ps5irq5NfrxwQUnVZD",
   authDomain: `${firebaseProjectName}.firebaseapp.com`,
   databaseURL: `https://${firebaseProjectName}.firebaseio.com`,
   projectId: `${firebaseProjectName}`,
